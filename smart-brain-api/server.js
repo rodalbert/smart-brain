22eabfe416ac552964516a955ddfc904b30f9331
RODRIGO ALBERTO <rodalbert@RODRIGOs-MBP.lan>
2019-05-05 12:58:20 +0100

    updating data base

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-amorphous-68602',
     user : 'rodalbert',
     password : '',
     database : 'smart-brain'
